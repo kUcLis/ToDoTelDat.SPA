352094a79e27cf878a8b2cde611eeff9ce6de03d
kUcLis <ulis89@gmail.com>
2024-10-18 16:13:16 +0200

    Refresher

--- src/app/Shared/Services/state.service.ts
+++ src/app/Shared/Services/state.service.ts
@@ -1,17 +1,32 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subscription, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StateService {
+export class StateService implements OnDestroy {
+  private subskrypcja: Subscription;
+  first = true;
   stateRefreshSubject = new BehaviorSubject<boolean>(false);
   refresh$ = this.stateRefreshSubject.asObservable();
 
   stateFormSubject = new BehaviorSubject<boolean>(false);
   form$ = this.stateFormSubject.asObservable();
 
-  constructor() { }
+  constructor() {
+    this.subskrypcja = timer(0, 300000).subscribe(() => {
+      if(!this.first){
+        this.setRefresh();
+      } else {
+        this.first = false;
+      }
+        
+    });
+   }
+
+  ngOnDestroy() {
+    this.subskrypcja.unsubscribe();
+  }
 
   setRefresh(){
     this.stateRefreshSubject.next(true);
